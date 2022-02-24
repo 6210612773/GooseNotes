e6a52983c35552f2beb793e876ff361d74633e14
Pathakorn Suksaeng <6210612773@student.tu.ac.th>
2022-02-24 23:57:44 +0700

    first try

---
+++ app/src/main/java/com/example/gooseNotes/ui/detail/ListItemViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.gooseNotes.ui.detail
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gooseNotes.databinding.ListItemViewHolderBinding
+
+class ListItemViewHolder(val binding: ListItemViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
